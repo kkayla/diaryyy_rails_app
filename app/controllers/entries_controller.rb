b4c0007a0646d4e88a36a8b0328ecaa4c6988d74
Kayla Williams <kkayla.williams@icloud.com>
2016-01-13 21:44:10 -0500

    Some baby steps are smaller than others - said Kayla

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -5,4 +5,18 @@ class EntriesController < ApplicationController
   def show
     @entry = Entry.find(params[:id])
   end
+  def create
+    entry_params = params["entry"].permit("title", "contents")
+    @entry = Entry.create(entry_params)
+    redirect_to entries_path
+end
+  def edit
+    @entry = Entry.find(params[:id])
+  end
+  def update
+    entry_params = params["entry"].permit("title", "contents")
+    @entry = Entry.find(params[:id])
+    entry.update(entry_params)
+    redirect_to entry_path
+end
 end
